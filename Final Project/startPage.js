fb2333307127b96247305ebf86f5855053a1d23b
FridayStark <yimeng.elsa.liu@outlook.com>
2022-12-13 18:15:30 -0500

    updated class

---
+++ Final Project/startPage.js
@@ -0,0 +1,54 @@
+class startPage{
+	
+	constructor(){
+		this.title = 120;
+		this.subtitle = 50;
+		this.caption = 30;
+	}
+	
+	menu(){
+	background(0);
+	fill(255);
+	textFont(fontBold);
+	textSize(this.title);
+	textAlign(CENTER);
+	text('Agent Jellyfish',width/2, height/2-250);
+	
+	textSize(this.subtitle);
+	text('press enter to start your journey',width/2,height/2+250);
+		
+	textFont(fontReg);
+	textSize(this.caption);
+	text('press i for instructions', width/2, height/2+300);
+		
+		
+	//clear();
+	//jellyfish.debug = mouse.pressing();
+	jellyfish.ani = 'stationary'; 
+	}
+	
+	instructions(){
+		background(0);
+		fill(255);
+		textAlign(CENTER);
+		textFont(fontBold);
+		textSize(this.title);
+		text('Instructions',width/2,height/2-320);
+		
+		textSize(this.subtitle);
+		text('press m to return to the menu',width/2,height-70);
+		
+		textFont(fontReg);
+		textSize(this.caption);
+		text("I wouldn't tell you what is going on here.",width/2,height/2-250);
+		text('Just press wasd keys to control your movement.', width/2,height/2-200);
+		text('Press space when you want to interact with anything or anyone.', width/2,height/2+200);
+		text('Hope you enjoy your journey......',width/2,height/2+250);
+		
+		//clear();
+		//jellyfish.debug = mouse.pressing();
+		jellyfish.ani = 'stationary'; 
+		
+	}
+	
+}
